57f1fb538bcc3608b72cd39da085735dff9c2f4d
Gkrbs <martian97@naver.com>
2022-03-10 18:52:18 +0900

    controlScript
    컨트롤스크립트 업데이트

--- VoiceFighting/Assets/Scripts/PlayerController.cs
+++ VoiceFighting/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float walkSpeed;
 
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotationX = 0;
+
+    [SerializeField]
+    private Camera theCamera;
+
     private Rigidbody myRidgid;
 
     // Start is called before the first frame update
     void Start()
     {
-
         myRidgid = GetComponent<Rigidbody>();
         
     }
@@ -26,11 +32,11 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        float _moveDirX = Input.GetAxisRaw("Horizontal");
-        float _moveDirZ = Input.GetAxisRaw("vertical");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        Vector3 _moveHorizontal = transform.right * _moveDirX;
-        Vector3 _moveVertical = transform.forward * _moveDirZ;
+        Vector3 _moveHorizontal = transform.right * x;
+        Vector3 _moveVertical = transform.forward * z;
 
         Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
 
