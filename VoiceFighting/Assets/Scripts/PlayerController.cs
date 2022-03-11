ef56b98375ac1c430547152e1115ef1289e47f3d
Gkrbs <martian97@naver.com>
2022-03-11 20:34:14 +0900

    ControllerTest
    플레이어컨트롤러 테스트

--- VoiceFighting/Assets/Scripts/PlayerController.cs
+++ VoiceFighting/Assets/Scripts/PlayerController.cs
@@ -4,43 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator anim;
 
-    [SerializeField]
-    private float walkSpeed;
-
-    [SerializeField]
-    private float cameraRotationLimit;
-    private float currentCameraRotationX = 0;
-
-    [SerializeField]
-    private Camera theCamera;
-
-    private Rigidbody myRidgid;
+    [SerializedFeild]
+    private float moveSpeed;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        myRidgid = GetComponent<Rigidbody>();
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            anim.Play("Idle");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.Play("Run");
+        }
     }
-
-    private void Move()
+    void Move()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float DirX = Input.GetAxis("Horizontal");
+        float DirZ = Input.GetAxis("Vertical");
 
-        Vector3 _moveHorizontal = transform.right * x;
-        Vector3 _moveVertical = transform.forward * z;
+        Vector3 _moveHorizontal = transform.right * DirX;
+        Vector3 _moveVertical = transform.forward * DirZ;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _vel = (_moveHorizontal + _moveVertical).normalized;
 
-        myRidgid.MovePosition(transform.position + _velocity * Time.deltaTime);
+
 
     }
 }
