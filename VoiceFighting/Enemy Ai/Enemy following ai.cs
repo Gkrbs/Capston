1b57bf0ddce73ebcdcd6f9ef548b6332deb4e29a
lee970845 <100989195+lee970845@users.noreply.github.com>
2022-04-02 13:16:11 +0900

    Update Enemy following ai.cs

--- VoiceFighting/Enemy Ai/Enemy following ai.cs
+++ VoiceFighting/Enemy Ai/Enemy following ai.cs
@@ -10,10 +10,12 @@ public class EnemyControll : MonoBehaviour
     private Transform _transform;
     private Transform playerTransform;
     private NavMeshAgent nvAgent;
-
+    static Animator anim2;
+    private Vector3 direction;
     
     void Start()
     {
+        anim2 = GetComponent<Animator>();
         _transform = this.gameObject.GetComponent<Transform>();
         playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
         nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
@@ -24,6 +26,8 @@ public class EnemyControll : MonoBehaviour
    
     void Update()
     {
-        
+        direction = playerTransform - this.transform.position;
+        direction.y = 0;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction),0.3f);
     }
 }
