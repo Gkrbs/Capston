82d3d2526a3f300335047ed2cff7659b34fe8310
lee970845 <100989195+lee970845@users.noreply.github.com>
2022-05-12 18:05:13 +0900

    Update EnemyControll.cs

--- VoiceFighting/EnemyControll.cs
+++ VoiceFighting/EnemyControll.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class EnemyControll : MonoBehaviour
 {
 
-    public enum CurrentState { idle, trace, attack, dead };
+    public enum CurrentState { idle, trace, kick, punch, dead };
     public CurrentState curState = CurrentState.trace;
   
 
@@ -14,9 +14,11 @@ public class EnemyControll : MonoBehaviour
     private Transform playerTransform;
     private NavMeshAgent nvAgent;
     private Animator _animator;
+    private Vector3 direction;
 
     public float traceDist = 15.0f;
-    public float attackDist = 2.5f;
+    public float kickDist = 2.0f;
+    public float punchDist = 1.0f;
     private bool isDead = false;
 
     void Start()
@@ -26,6 +28,10 @@ public class EnemyControll : MonoBehaviour
         playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
         nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
         _animator = this.gameObject.GetComponent<Animator>();
+
+        direction = playerTransform.position - this.transform.position;
+        direction.y = 0;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.3f);
 
         StartCoroutine(this.CheckState());
         StartCoroutine(this.CheckStateForAction());
@@ -41,10 +47,14 @@ public class EnemyControll : MonoBehaviour
 
             float dist = Vector3.Distance(playerTransform.position, _transform.position);
 
-            if (dist <= attackDist)
+            if (dist <= punchDist)
             {
-                curState = CurrentState.attack;
+                curState = CurrentState.punch;
 
+            }
+            else if (dist <= kickDist)
+            {
+                curState = CurrentState.kick;
             }
 
             else if (dist <= traceDist)
@@ -75,10 +85,31 @@ public class EnemyControll : MonoBehaviour
                     _animator.SetBool("IsTrace", true);
                     
                     break;
-                case CurrentState.attack:
-                    int a= Random.Range(0, 4);
+                case CurrentState.kick:
+                    int a= Random.Range(0, 2);
 
                     switch(a)
+                    {
+                        case 0:
+                            _animator.SetBool("Isleftkick", true);
+                            yield return new WaitForSeconds(2.5f);
+                            _animator.SetBool("Isleftkick", false);
+                            break;
+                        case 1:
+                            _animator.SetBool("Isrightkick", true);
+
+                            yield return new WaitForSeconds(2.5f);
+
+                            _animator.SetBool("Isrightkick", false);
+
+                            break;
+                        
+                    }
+                    break;
+                case CurrentState.punch:
+                    int b = Random.Range(0, 2);
+
+                    switch (b)
                     {
                         case 0:
                             _animator.SetBool("Islefthook", true);
@@ -86,30 +117,17 @@ public class EnemyControll : MonoBehaviour
                             _animator.SetBool("Islefthook", false);
                             break;
                         case 1:
-                            _animator.SetBool("Isrightkick", true);
-
-                            yield return new WaitForSeconds(1.8f);
-
-                            _animator.SetBool("Isrightkick", false);
-
-                            break;
-                        case 2:
-                            _animator.SetBool("Isleftkick", true);
-
-                            yield return new WaitForSeconds(1.8f);
-
-                            _animator.SetBool("Isleftkick", false);
-
-                            break;
-                        case 3:
                             _animator.SetBool("Isrightpunch", true);
+
                             yield return new WaitForSeconds(2.5f);
+
                             _animator.SetBool("Isrightpunch", false);
+
                             break;
 
                     }
                     break;
-                    
+
             }
             
 
@@ -122,4 +140,3 @@ public class EnemyControll : MonoBehaviour
        
     }
 }
-
